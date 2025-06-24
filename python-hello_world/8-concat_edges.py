f4e1c805a5ba96737f378dd90b572c2046733aab
Natinael Boda <n.borana@alustudent.com>
2025-06-24 06:13:54 -0700

    Script 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[94:100] + str[:6]
+str = str[39:66] + str[80:86] + str[:6]
 print(str)
