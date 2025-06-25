1e9b6b130dbb8fe7a3b9811cf15dbe86cefa6efc
Natinael-1 <n.borana@alustudent.com>
2025-06-25 08:04:40 -0700

    Script 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i) + (", " if i < 99 else ""))
-
+for number in range(100):
+    if number < 99:
+        print("{:02d}".format(number), end=", ")
+    else:
+        print("{:02d}".format(number))
