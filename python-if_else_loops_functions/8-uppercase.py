6e3ddca4e630c47367d22619b73940d4ecc6737e
Natinael-1 <n.borana@alustudent.com>
2025-06-25 06:12:23 -0700

    Script 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in uppercase followed by a new line."""
     result = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:  # Check if lowercase
