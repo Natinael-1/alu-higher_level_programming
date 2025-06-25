aa5560128f4e47f121aa49fff2c282c999ae511a
Natinael-1 <n.borana@alustudent.com>
2025-06-25 07:46:24 -0700

    Script 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+        if i % 15 == 0:
+            print("FizzBuzz", end="")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz", end="")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            print("Buzz", end="")
         else:
-            print(i, end=" ")
-    print("")
+            print(i, end="")
+        if i != 100:  # Only print space if not last number
+            print(" ", end="")
+    print()  # Single newline at the end
 
 fizzbuzz()
