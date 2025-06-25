3dcb050beec7ffcf2c1afc3a081f5bd06e375024
Natinael-1 <n.borana@alustudent.com>
2025-06-25 06:36:37 -0700

    Script 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in uppercase followed by a new line using string format."""
+    """Print a string in uppercase followed by a new line."""
     result = ""
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):  # Check if lowercase
-            result += chr(ord(c) - 32)       # Convert to uppercase
+        if 97 <= ord(c) <= 122:
+            result += chr(ord(c) - 32)
         else:
             result += c
-    print("{}".format(result))  # Use .format() to satisfy autograder
+    print("{}".format(result))
