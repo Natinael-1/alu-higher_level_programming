1b350bdc5d50239decd643fead099305e483cf1f
Natinael-1 <n.borana@alustudent.com>
2025-06-26 11:39:47 -0700

    Script 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def element_at(my_list, idx):
+    for i in my_list:
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
+            return my_list[idx]
+def access_value():
+    a = [2,4,6,8,10]
+    print(element_at(a, -1))
+access_value()
