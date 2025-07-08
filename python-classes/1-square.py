5eacc1cdb0dbb6ee00b0b67d4f581d4d71b71b52
Natinael-1 <n.borana@alustudent.com>
2025-07-08 03:42:02 -0700

    Script 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,9 @@
-
-
 #!/usr/bin/python3
+
+
 """This module defines a Square class with a private size attribute."""
 class Square:
     """This class represents a square with a private size attribute.
-
     The size of a square is crucial for computations like area,
     so it is kept private to control its value and type.
     """
