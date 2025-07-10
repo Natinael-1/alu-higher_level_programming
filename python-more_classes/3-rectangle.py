04a20c906960f63eb7aa8d333655be87943a767e
Natinael-1 <n.borana@alustudent.com>
2025-07-10 02:41:11 -0700

    Script 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Defines a rectangle with width, height, area, perimeter,
     and string representation"""
-    
+
     def __init__(self, width=0, height=0):
         """Initializes the rectangle with optional width and height"""
         self.width = width
