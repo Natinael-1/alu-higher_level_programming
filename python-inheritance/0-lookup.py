cc3a1b2c129b357fab6398d6f4c00349d205085a
Natinael-1 <n.borana@alustudent.com>
2025-07-11 06:07:49 -0700

    Script 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module provides a function that returns a list of attributes and methods."""
+""" module provides function that returns a list of attributes and methods."""
 
 
 def lookup(obj):
