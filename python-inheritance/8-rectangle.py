3afd68bc62c4eda128dd7218d210d50e572ebae9
Natinael-1 <n.borana@alustudent.com>
2025-07-11 12:19:38 -0700

    Script 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""module defines Rectangle that inherits from BaseGeometry."""
+"""This module defines a Rectangle that inherits from BaseGeometry."""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
